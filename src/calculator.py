b1c19eeaf49bf9ff3636d1ff7ae74fa1754550b5
Brijmohan Gupta <iambrijbabu@gmail.com>
2025-09-28 15:18:30 +0530

    added remaining test cases

--- src/calculator.py
+++ src/calculator.py
@@ -2,4 +2,20 @@ import re
 
 
 def add(s: str) -> int:
-    return sum(list(map(int, s.split(','))))
+    delimiter = re.search('^//(.*)\\n', s)
+
+    if delimiter:
+        s = re.sub("^//(.*)\\n", "", s)
+        s = s.replace(delimiter.groups()[0], ",")
+
+    s = s.replace("\n", ",")
+
+    if not s:
+        return 0
+
+    if "-" in s:
+        negative_nos = re.findall('(-\\d+)', s)
+        raise ValueError(f"negative numbers not allowed {','.join(negative_nos)}")
+
+    numbers = list(map(int, s.split(',')))
+    return sum(numbers)
--- test/test_calculator.py
+++ test/test_calculator.py
@@ -1,8 +1,22 @@
 import os.path
 import sys
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+import pytest
 
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from src.calculator import add
+
+def test_add_no_digit():
+    assert add("") == 0
 
 def test_add():
     assert add("1,2") == 3
+
+def test_add_new_line():
+    assert add("1\n2,3") == 6
+
+def test_add_new_delimiter():
+    assert add("//;\n1;2") == 3
+
+def test_add_negatives():
+    with pytest.raises(ValueError, match="negative numbers not allowed -1000,-2"):
+        add("//;\n-1000;-2")
